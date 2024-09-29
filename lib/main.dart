01f9d193ab30d1f2dbf903a7a635d85b42625ed0
Kacper Żegleń <kacper.zeglen@sap.com>
2024-09-29 02:27:52 +0200

    logo added

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,35 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-// Make MyApp a StatefulWidget
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  // MapController should be in the State class to manage it properly
   late MapController mapController;
-  
-  // Focus nodes to manage focus between text fields
+
   final FocusNode focusNode1 = FocusNode();
   final FocusNode focusNode2 = FocusNode();
 
-  // Controllers for text fields to modify text programmatically
   final TextEditingController textController1 = TextEditingController();
   final TextEditingController textController2 = TextEditingController();
 
   GeoPoint? lastPoint1;
   GeoPoint? lastPoint2;
 
-  // Variable to track the last active TextField
   FocusNode? lastActiveFocusNode;
 
-  // Method to input data into the last active TextField
+  double textFieldHeight = 48.0; // Default height for the TextField
+
   void inputDataToLastActiveField(String data) {
     if (lastActiveFocusNode == focusNode1) {
       textController1.text = data;
@@ -45,7 +42,6 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    // Initialize the mapController in initState
     mapController = MapController(
       initPosition: GeoPoint(latitude: 50.0496833, longitude: 19.944544),
     );
@@ -53,41 +49,48 @@ class _MyAppState extends State<MyApp> {
     mapController.listenerMapSingleTapping.addListener(() {
       if (mapController.listenerMapSingleTapping.value != null) {
         var data = mapController.listenerMapSingleTapping.value!;
-        
 
-        if(lastActiveFocusNode == focusNode1){
-          if(lastPoint1 == null){
-            mapController.addMarker(data, markerIcon: const MarkerIcon(icon: Icon(
-              Icons.location_on, // Use the built-in location icon
-              color: Colors.blue,  // Set the color to red
-              size: 50.0,        // Adjust the size as needed
-            ),));
-          } else{
+        if (lastActiveFocusNode == focusNode1) {
+          if (lastPoint1 == null) {
+            mapController.addMarker(
+              data,
+              markerIcon: const MarkerIcon(
+                icon: Icon(
+                  Icons.location_on,
+                  color: Colors.blue,
+                  size: 50.0,
+                ),
+              ),
+            );
+          } else {
             mapController.changeLocationMarker(oldLocation: lastPoint1!, newLocation: data);
           }
           inputDataToLastActiveField("${data.latitude} ${data.longitude}");
           lastPoint1 = data;
-        } else if (lastActiveFocusNode == focusNode2){
-          if(lastPoint2 == null){
-            mapController.addMarker(data, markerIcon: const MarkerIcon(icon: Icon(
-              Icons.location_on, // Use the built-in location icon
-              color: Colors.red,  // Set the color to red
-              size: 50.0,        // Adjust the size as needed
-            ),));
-          } else{
+        } else if (lastActiveFocusNode == focusNode2) {
+          if (lastPoint2 == null) {
+            mapController.addMarker(
+              data,
+              markerIcon: const MarkerIcon(
+                icon: Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 50.0,
+                ),
+              ),
+            );
+          } else {
             mapController.changeLocationMarker(oldLocation: lastPoint2!, newLocation: data);
           }
           inputDataToLastActiveField("${data.latitude} ${data.longitude}");
           lastPoint2 = data;
+        }
       }
-    }
-  });
-  
+    });
   }
 
   @override
   void dispose() {
-    // Dispose of the controllers and focus nodes when no longer needed
     textController1.dispose();
     textController2.dispose();
     focusNode1.dispose();
@@ -98,100 +101,128 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'XD',
+      title: 'CycloSafe Navigator',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('CycloSafe Navigator'),
         ),
-        body: Row(
+        body: Stack(
           children: [
-            // First column taking 20% of the space
-            Flexible(
-              flex: 2, // 2 parts out of 10 (20%)
-              child: Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView(
-                    children: [
-                      // First Text Input
-                      TextField(
-                        focusNode: focusNode1,
-                        controller: textController1,
-                        onTap: () {
-                          // Set this TextField as the last active one when tapped
-                          setState(() {
-                            lastActiveFocusNode = focusNode1;
-                          });
-                        },
-                        decoration: const InputDecoration(
-                          labelText: 'Input 1',
-                          border: OutlineInputBorder(),
+            Row(
+              children: [
+                // First column taking 20% of the space
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          focusNode: focusNode1,
+                          controller: textController1,
+                          onTap: () {
+                            setState(() {
+                              lastActiveFocusNode = focusNode1;
+                            });
+                          },
+                          decoration: const InputDecoration(
+                            labelText: 'Input 1',
+                            border: OutlineInputBorder(),
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 16), // Space between inputs
-                      // Second Text Input
-                      TextField(
-                        focusNode: focusNode2,
-                        controller: textController2,
-                        onTap: () {
-                          // Set this TextField as the last active one when tapped
-                          setState(() {
-                            lastActiveFocusNode = focusNode2;
-                          });
-                        },
-                        decoration: const InputDecoration(
-                          labelText: 'Input 2',
-                          border: OutlineInputBorder(),
+                        const SizedBox(height: 16), // Space between inputs
+                        // Second Text Input
+                        TextField(
+                          focusNode: focusNode2,
+                          controller: textController2,
+                          onTap: () {
+                            setState(() {
+                              lastActiveFocusNode = focusNode2;
+                            });
+                          },
+                          decoration: const InputDecoration(
+                            labelText: 'Input 2',
+                            border: OutlineInputBorder(),
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 16), // Space before button
-                      // Button
-                      ElevatedButton(
-                        onPressed: () {
-                          // Add your button logic here
-                        },
-                        child: const Text('Submit'),
-                      ),
-                    ],
+                        const SizedBox(height: 16), // Space before button
+                        // Button
+                        ElevatedButton(
+                          onPressed: () {
+                            // Add your button logic here
+                          },
+                          child: const Text('Submit'),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+                // Second column taking 80% of the space
+                Expanded(
+                  flex: 8,
+                  child: OSMFlutter(
+                    controller: mapController,
+                    osmOption: OSMOption(
+                      userTrackingOption: const UserTrackingOption(
+                        enableTracking: true,
+                        unFollowUser: false,
+                      ),
+                      zoomOption: const ZoomOption(
+                        initZoom: 13,
+                        minZoomLevel: 13,
+                        maxZoomLevel: 19,
+                        stepZoom: 1.0,
+                      ),
+                      userLocationMarker: UserLocationMaker(
+                        personMarker: const MarkerIcon(
+                          icon: Icon(
+                            Icons.location_history_rounded,
+                            color: Colors.red,
+                            size: 48,
+                          ),
+                        ),
+                        directionArrowMarker: const MarkerIcon(
+                          icon: Icon(
+                            Icons.double_arrow,
+                            size: 48,
+                          ),
+                        ),
+                      ),
+                      roadConfiguration: const RoadOption(
+                        roadColor: Colors.yellowAccent,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            // Second column taking 80% of the space
-            Expanded(
-              flex: 8, // 8 parts out of 10 (80%)
-              child: OSMFlutter(
-                controller: mapController,
-                osmOption: OSMOption(
-                  userTrackingOption: const UserTrackingOption(
-                    enableTracking: true,
-                    unFollowUser: false,
-                  ),
-                  zoomOption: const ZoomOption(
-                    initZoom: 13,
-                    minZoomLevel: 13,
-                    maxZoomLevel: 19,
-                    stepZoom: 1.0,
-                  ),
-                  userLocationMarker: UserLocationMaker(
-                    personMarker: const MarkerIcon(
-                      icon: Icon(
-                        Icons.location_history_rounded,
-                        color: Colors.red,
-                        size: 48,
+            // Positioned SVG in the bottom left corner with dynamic scaling
+            Positioned(
+              left: MediaQuery.of(context).size.width * 0, // Scale padding based on screen width
+              bottom: MediaQuery.of(context).size.height * 0, // Scale padding based on screen height
+              child: Builder(
+                builder: (context) {
+                  // Calculate the size and total space required for the SVG
+                  double svgSize = MediaQuery.of(context).size.width * 0.2;
+                  double bottomPadding = MediaQuery.of(context).size.height * 0.01;
+                  double svgTotalHeight = svgSize + bottomPadding;
+
+                  // Determine if the SVG can fit without covering the text fields or button
+                  bool shouldDisplaySvg = MediaQuery.of(context).size.height - svgTotalHeight > 200;
+
+                  return Visibility(
+                    visible: shouldDisplaySvg,
+                    child: Container(
+                      color: Colors.transparent,
+                      padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.01),
+                      child: SvgPicture.asset(
+                        'assets/logo2_final.svg',
+                        width: svgSize,
+                        height: svgSize,
                       ),
                     ),
-                    directionArrowMarker: const MarkerIcon(
-                      icon: Icon(
-                        Icons.double_arrow,
-                        size: 48,
-                      ),
-                    ),
-                  ),
-                  roadConfiguration: const RoadOption(
-                    roadColor: Colors.yellowAccent,
-                  ),
-                ),
+                  );
+                },
               ),
             ),
           ],
